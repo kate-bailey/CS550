d85a91e7874f0dd4c252d10fb656db3b75d09153
Kate Bailey <40126696+kate-bailey@users.noreply.github.com>
2019-09-06 13:47:44 -0400

    Add files via upload

---
+++ HW_conversation.py
@@ -0,0 +1,15 @@
+# Kate Bailey
+# September 6, 2019
+# This program allows the user to have a conversation with their computer, basically.
+# Sources: None
+# OMH
+print('\nHey there!')
+name = input("You're new here, right? What's your name again?\n")
+course = input("Nice to meet you, "+name+". What class do you have next?\n")
+teacher = input("I've heard "+course+" is fun — who do you have?\n")
+print("No way! I had "+teacher+" last year. (S)he's the best.")
+dorm = input("Which dorm do you live in?\n")
+print("Ooh, if you're living in "+dorm+", make sure to always sign REACH. They're super strict about that stuff.")
+sport = input("Which sport are you playing this term?\n")
+print("I didn't even know we had "+sport+" at Choate. That's super cool!")
+print("Well, here's my classroom. See ya later, "+name+"!")
